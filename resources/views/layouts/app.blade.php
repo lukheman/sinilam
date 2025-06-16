1fb6af394678ddc047eabd3839807ef06c72eb44
Akmal <akmalthemalong@gmail.com>
2025-06-16 14:54:33 +0800

    feat: title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Material UI Style Admin Dashboard - Bootstrap 5</title>
+  <title>Sinilam | {{ $title ?? config('app.name') }}</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
